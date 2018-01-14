aad2b8d09e8785d6045b62e6a3e7d379c24d4cb5
Chewsterchew <chew@chew.pw>
2018-01-13 21:22:26 -0600

    beautify and make room for acronym api

--- server.js
+++ server.js
@@ -2,22 +2,27 @@
 
 var express = require('express');
 var trbmb = require('./trbmb');
+var acronym = require('./acronym');
 var app = express();
 var port = process.env.PORT || 3000;
 
-app.all('*', function (req, res, next) {
-	res.set('Access-Control-Allow-Origin', '*');
-	next();
+app.all('*', function(req, res, next) {
+  res.set('Access-Control-Allow-Origin', '*');
+  next();
 });
 
-app.get('/trbmb/', function (req, res) {
-	res.send(trbmb.getRandom());
+app.get('/trbmb/', function(req, res) {
+  res.send(trbmb.getRandom());
 });
 
-app.get('/', function (req, res) {
-	res.send('<meta http-equiv="refresh" content="0; url=http://github.com/Chewsterchew/API" /><p>Not refreshing? Click <a href="http://github.com/Chewsterchew/API">here</a>.</p>');
+app.get('/', function(req, res) {
+  res.send('<meta http-equiv="refresh" content="0; url=http://github.com/Chewsterchew/API" /><p>Not refreshing? Click <a href="http://github.com/Chewsterchew/API">here</a>.</p>');
 });
 
-app.listen(port, function () {
-	console.log('Server running on port', port);
+app.get('/acronym/:acronym', function(req, res) {
+  res.send(acronym.genAcro(req.params))
+});
+
+app.listen(port, function() {
+  console.log('Server running on port', port);
 })
